2b5a966c63317f344441c7df0ba4369986a661e8
Fathin Mubarak <fathin.mubarak@gmail.com>
2025-04-27 21:25:20 +0700

    add homepage

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Filament\Resources\MouResource\Pages\CostListMou;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
