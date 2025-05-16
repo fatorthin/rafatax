34bdd512a18f52c2362d490af3b930ab6a04bca9
Fathin Mubarak <fathin.m@staff.uns.ac.id>
2025-05-16 11:08:14 +0700

    edit homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,7 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="TemplateMo">
+    <meta name="author" content="Rafatax Consulting">
+
+    <link rel="icon" href="{{ asset('images/logo.svg') }}" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <title>Rafatax</title>
@@ -64,7 +66,7 @@ https://templatemo.com/tm-545-finance-business
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="index.html"><h2>Finance Business</h2></a>
+          <a class="navbar-brand" href="index.html"><h2>Rafatax Consulting</h2></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
