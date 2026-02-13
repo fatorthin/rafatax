bfffb4f26244c8f6426c296b10a8789da2368927
Fathin Mubarak <fathin.mubarak@gmail.com>
2026-02-13 14:10:14 +0700

    update modul mou

---
+++ app/Filament/App/Resources/MouResource/Widgets/MouStatsOverview.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Filament\App\Resources\MouResource\Widgets;
+
+use App\Models\CostListMou;
+use App\Models\CostListInvoice;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class MouStatsOverview extends BaseWidget
+{
+    public ?int $mouId = null;
+
+    protected function getStats(): array
+    {
+        $totalCostListMou = CostListMou::where('mou_id', $this->mouId)->sum('total_amount');
+
+        $totalCostListInvoiceUnpaid = CostListInvoice::where('mou_id', $this->mouId)
+            ->whereNotNull('invoice_id')
+            ->whereHas('invoice', function ($query) {
+                $query->where('invoice_status', 'unpaid');
+            })
+            ->sum('amount');
+
+        $totalCostListInvoicePaid = CostListInvoice::where('mou_id', $this->mouId)
+            ->whereNotNull('invoice_id')
+            ->whereHas('invoice', function ($query) {
+                $query->where('invoice_status', 'paid');
+            })
+            ->sum('amount');
+
+        $difference = $totalCostListMou - ($totalCostListInvoiceUnpaid + $totalCostListInvoicePaid);
+
+        return [
+            Stat::make('Total Cost List MoU', 'Rp ' . number_format($totalCostListMou, 0, ',', '.'))
+                ->description('Total anggaran dalam MoU')
+                ->descriptionIcon('heroicon-m-document-text')
+                ->color('info'),
+
+            Stat::make('Total Invoice Unpaid', 'Rp ' . number_format($totalCostListInvoiceUnpaid, 0, ',', '.'))
+                ->description('Total tagihan yang belum dibayar')
+                ->descriptionIcon('heroicon-m-clock')
+                ->color('warning'),
+
+            Stat::make('Total Invoice Paid', 'Rp ' . number_format($totalCostListInvoicePaid, 0, ',', '.'))
+                ->description('Total tagihan yang sudah dibayar')
+                ->descriptionIcon('heroicon-m-check-circle')
+                ->color('success'),
+
+            Stat::make('Selisih', 'Rp ' . number_format(abs($difference), 0, ',', '.'))
+                ->description($difference >= 0 ? 'Sisa anggaran' : 'Melebihi anggaran')
+                ->descriptionIcon($difference >= 0 ? 'heroicon-m-arrow-trending-down' : 'heroicon-m-arrow-trending-up')
+                ->color($difference >= 0 ? 'warning' : 'danger'),
+        ];
+    }
+}
--- resources/views/filament/app/resources/mou-resource/pages/list-cost-mou.blade.php
+++ resources/views/filament/app/resources/mou-resource/pages/list-cost-mou.blade.php
@@ -1,5 +1,9 @@
 <x-filament-panels::page>
     <div class="space-y-6">
+        <div class="rounded-xl">
+            @livewire(App\Filament\App\Resources\MouResource\Widgets\MouStatsOverview::class, ['mouId' => $this->mou->id])
+        </div>
+
         <!-- MoU Information -->
         {{ $this->infolist }}
 
