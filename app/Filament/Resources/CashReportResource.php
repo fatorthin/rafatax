59bd02579d7dcc877c163dd5f9b4487cbe08f54f
Fathin Mubarak <fathin.m@staff.uns.ac.id>
2026-06-19 14:56:15 +0700

    update modul cash

--- app/Filament/Resources/CashReportResource.php
+++ app/Filament/Resources/CashReportResource.php
@@ -13,6 +13,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Notifications\Notification;
 
 class CashReportResource extends Resource
 {
