c8b1ed942fbb3dd4b0dbe47bda48a5e5faf70da1
Fathin Mubarak <fathin.m@staff.uns.ac.id>
2026-08-22 11:28:08 +0700

    update

--- resources/views/filament/resources/cash-report-resource/pages/neraca-lajur-bulanan.blade.php
+++ resources/views/filament/resources/cash-report-resource/pages/neraca-lajur-bulanan.blade.php
@@ -2,11 +2,70 @@
     <div class="space-y-6">
         <!-- Header dengan periode -->
 
-
         <!-- Tabel Neraca Lajur -->
-        <div class="shadow rounded-lg overflow-hidden">
-            @php
-                $data = $this->getTableQuery()->get();
+        <div 
+            x-data="{
+                tableEl: null,
+                scrollWidth: 0,
+                hasOverflow: false,
+                init() {
+                    this.$nextTick(() => this.syncScrollbar());
+                    Livewire.hook('commit', ({ succeed }) => {
+                        succeed(() => {
+                            this.$nextTick(() => this.syncScrollbar());
+                        });
+                    });
+                    window.addEventListener('resize', () => this.syncScrollbar());
+                },
+                syncScrollbar() {
+                    const container = this.$refs.tableWrapper.querySelector('.table-container, [class*=\'table-container\']');
+                    if (!container) return;
+                    this.tableEl = container;
+                    this.scrollWidth = container.scrollWidth;
+                    this.hasOverflow = container.scrollWidth > container.clientWidth;
+
+                    container.onscroll = () => {
+                        if (this.$refs.topScroll && Math.abs(this.$refs.topScroll.scrollLeft - container.scrollLeft) > 1) {
+                            this.$refs.topScroll.scrollLeft = container.scrollLeft;
+                        }
+                    };
+
+                    if (window.ResizeObserver && !this.observer) {
+                        this.observer = new ResizeObserver(() => {
+                            if (this.tableEl) {
+                                this.scrollWidth = this.tableEl.scrollWidth;
+                                this.hasOverflow = this.tableEl.scrollWidth > this.tableEl.clientWidth;
+                            }
+                        });
+                        this.observer.observe(container);
+                        const table = container.querySelector('table');
+                        if (table) this.observer.observe(table);
+                    }
+                },
+                onTopScroll(e) {
+                    if (this.tableEl && Math.abs(this.tableEl.scrollLeft - e.target.scrollLeft) > 1) {
+                        this.tableEl.scrollLeft = e.target.scrollLeft;
+                    }
+                }
+            }" 
+            class="space-y-2"
+        >
+            <!-- Bilah Scrollbar Horizontal Atas -->
+            <div 
+                x-ref="topScroll"
+                @scroll="onTopScroll"
+                x-show="hasOverflow"
+                x-cloak
+                class="overflow-x-auto overflow-y-hidden rounded-xl bg-gray-100/90 dark:bg-gray-800/80 h-3.5 border border-gray-200/90 dark:border-gray-700 shadow-inner transition-all no-print"
+                style="scrollbar-width: thin;"
+                title="Geser scroll horizontal tabel"
+            >
+                <div :style="'width: ' + scrollWidth + 'px; height: 1px;'"></div>
+            </div>
+
+            <div x-ref="tableWrapper" class="shadow rounded-lg overflow-hidden">
+                @php
+                    $data = $this->getTableQuery()->get();
 
                 // Initialize all total variables
                 $totalNeracaAwalDebit = 0;
@@ -519,6 +578,7 @@
                 </table>
             </div>
         </div>
+    </div>
 
         <style>
             @media print {
--- resources/views/filament/resources/cash-report-resource/pages/neraca-lajur-piutang.blade.php
+++ resources/views/filament/resources/cash-report-resource/pages/neraca-lajur-piutang.blade.php
@@ -22,9 +22,69 @@
         </div>
 
         {{-- Tabel Neraca Lajur --}}
-        <div class="shadow rounded-lg overflow-hidden">
-            @php
-                $data = $this->getTableData();
+        <div 
+            x-data="{
+                tableEl: null,
+                scrollWidth: 0,
+                hasOverflow: false,
+                init() {
+                    this.$nextTick(() => this.syncScrollbar());
+                    Livewire.hook('commit', ({ succeed }) => {
+                        succeed(() => {
+                            this.$nextTick(() => this.syncScrollbar());
+                        });
+                    });
+                    window.addEventListener('resize', () => this.syncScrollbar());
+                },
+                syncScrollbar() {
+                    const container = this.$refs.tableWrapper.querySelector('.table-container-piutang, .table-container, [class*=\'table-container\']');
+                    if (!container) return;
+                    this.tableEl = container;
+                    this.scrollWidth = container.scrollWidth;
+                    this.hasOverflow = container.scrollWidth > container.clientWidth;
+
+                    container.onscroll = () => {
+                        if (this.$refs.topScroll && Math.abs(this.$refs.topScroll.scrollLeft - container.scrollLeft) > 1) {
+                            this.$refs.topScroll.scrollLeft = container.scrollLeft;
+                        }
+                    };
+
+                    if (window.ResizeObserver && !this.observer) {
+                        this.observer = new ResizeObserver(() => {
+                            if (this.tableEl) {
+                                this.scrollWidth = this.tableEl.scrollWidth;
+                                this.hasOverflow = this.tableEl.scrollWidth > this.tableEl.clientWidth;
+                            }
+                        });
+                        this.observer.observe(container);
+                        const table = container.querySelector('table');
+                        if (table) this.observer.observe(table);
+                    }
+                },
+                onTopScroll(e) {
+                    if (this.tableEl && Math.abs(this.tableEl.scrollLeft - e.target.scrollLeft) > 1) {
+                        this.tableEl.scrollLeft = e.target.scrollLeft;
+                    }
+                }
+            }" 
+            class="space-y-2"
+        >
+            <!-- Bilah Scrollbar Horizontal Atas -->
+            <div 
+                x-ref="topScroll"
+                @scroll="onTopScroll"
+                x-show="hasOverflow"
+                x-cloak
+                class="overflow-x-auto overflow-y-hidden rounded-xl bg-gray-100/90 dark:bg-gray-800/80 h-3.5 border border-gray-200/90 dark:border-gray-700 shadow-inner transition-all no-print"
+                style="scrollbar-width: thin;"
+                title="Geser scroll horizontal tabel"
+            >
+                <div :style="'width: ' + scrollWidth + 'px; height: 1px;'"></div>
+            </div>
+
+            <div x-ref="tableWrapper" class="shadow rounded-lg overflow-hidden">
+                @php
+                    $data = $this->getTableData();
 
                 $totalNeracaAwalDebit = 0;
                 $totalNeracaAwalKredit = 0;
@@ -517,6 +577,7 @@
                 </table>
             </div>
         </div>
+    </div>
     </div>
     <x-filament-actions::modals />
 </x-filament-panels::page>
